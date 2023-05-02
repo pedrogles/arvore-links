96715d51e6c3da2157d67439496efe85a2c900b7
Pedro Gabriel <pedrogabriellima@gmail.com>
2023-05-02 20:15:02 -0300

    build test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { navbar, user, links, buttons, projects } from "./utils/data";
 
-import Navbar from "./components/Navbar/Navbar";
-import User from "./components/User/User";
-import Link from "./components/Link/Link";
-import Button from "./components/Button/Button";
-import Projects from "./components/Projects/Projects";
+import Navbar from "./components/navbar/Navbar";
+import User from "./components/user/User";
+import Link from "./components/link/Link";
+import Button from "./components/button/Button";
+import Projects from "./components/projects/Projects";
 
 import "./styles/app.scss";
 

8792eb80b599f220e90e0e307beb66284c027871
Pedro Gabriel <pedrogabriellima@gmail.com>
2023-05-02 20:23:01 -0300

    adding components.js

---
+++ src/utils/components.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
+import User from "../components/user/User";
+import Link from "../components/link/Link";
+import Button from '../components/button/Button';
+import Projects from "../components/projects/Projects";
+
+const Components = {
+    Navbar,
+    User,
+    Link,
+    Button,
+    Projects
+};
+
+export default Components;

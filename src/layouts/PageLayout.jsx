ee5d985683ec4e2c7c0a03dd37826fbf39343ba9
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-09-16 11:08:11 -0300

    Style: PageLayout responsiveness

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -4,7 +4,7 @@ export function PageLayout({ children }) {
     return (
         <>
             <Navbar />
-            <main className="flex flex-col items-center bg-slate-50">
+            <main className="flex flex-col justify-center items-center gap-6 bg-slate-50 py-28 md:py-40 md:gap-8 lg:py-48">
                 {children}
             </main>
         </>

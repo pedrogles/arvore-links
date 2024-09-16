78350aeb3efa887e6e977b63f0c57590e4a165cb
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-09-16 11:13:02 -0300

    Refactor: Button refactoring 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { PageLayout } from "./layouts/PageLayout";
-import { User, Links, Buttons, Projects } from "./components/features";
+import { User, Buttons, Projects } from "./components/features";
 
 export default function App() {
   return (
     <PageLayout>
         <User />
-        <Links />
         <Buttons />
         <Projects />
     </PageLayout>
